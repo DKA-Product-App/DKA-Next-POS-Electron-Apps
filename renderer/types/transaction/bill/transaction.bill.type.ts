241b40566939a35f8490c7f4b550f8ced1ff378a
Yovangga Anandhika <dka.tech.dev@gmail.com>
2026-05-30 21:11:00 +0800

    style: refactor TransactionBill interface with consistent indentation and spacing

--- renderer/types/transaction/bill/transaction.bill.type.ts
+++ renderer/types/transaction/bill/transaction.bill.type.ts
@@ -3,32 +3,32 @@
  * Root: ApiTransactionBill
  * ======================================================= */
 
-import {Accounts} from "../../account/accounts.type";
-import {ConfigBranch} from "../../config/base/branch.type";
-import {TransactionBillItem} from "./transaction.bill.item.type";
-import {Transaction} from "../transaction.type";
-import {TransactionBillPaid} from "./transaction.bill.paid.type";
+import { Accounts } from "../../account/accounts.type";
+import { ConfigBranch } from "../../config/base/branch.type";
+import { TransactionBillItem } from "./transaction.bill.item.type";
+import { Transaction } from "../transaction.type";
+import { TransactionBillPaid } from "./transaction.bill.paid.type";
 
 /** ==== ROOT TYPE: ApiTransactionBill ==== */
 export interface TransactionBill {
-    id?: string;
-    bill?: string;              // ex: "396"
-    tax?: number;              // ex: 0.1
-    time_created?: string;
-    time_updated?: string;
-    time_deleted?: string | null;
-    reference?: Accounts;
-    branch?: ConfigBranch[];      // array pada payload
-    transaction?: Transaction;
-    items?: TransactionBillItem[];
-    paid?: TransactionBillPaid;
-    voucher_code?: string | null;
-    voucher_type?: "percentage" | "fixed" | null;
-    voucher_value?: number;
-    discount_amount?: number;
-    meta?: {
-        request_key?: string;
-    }
+  id?: string;
+  bill?: string;              // ex: "396"
+  tax?: number;              // ex: 0.1
+  time_created?: string;
+  time_updated?: string;
+  time_deleted?: string | null;
+  reference?: Accounts;
+  branch?: ConfigBranch[];      // array pada payload
+  transaction?: Transaction;
+  items?: TransactionBillItem[];
+  paid?: TransactionBillPaid;
+  voucher_code?: string | null;
+  voucher_type?: "percentage" | "fixed" | null;
+  voucher_value?: number;
+  discount_amount?: number;
+  meta?: {
+    request_key?: string;
+  }
 }
 
 export interface ApiTransactionBillResponse {
